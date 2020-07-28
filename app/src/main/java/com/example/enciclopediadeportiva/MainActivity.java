8a5d8a5c6a989527b1de87b6bbe25c59f1f1c7e1
Sergio Muro <sergio.muroc@pucp.pe>
2020-07-27 19:31:50 -0500

    login

--- app/src/main/java/com/example/enciclopediadeportiva/MainActivity.java
+++ app/src/main/java/com/example/enciclopediadeportiva/MainActivity.java
@@ -1,118 +1,101 @@
 package com.example.enciclopediadeportiva;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.content.Intent;
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
+import com.example.enciclopediadeportiva.ProfileActivity;
+
+import com.example.enciclopediadeportiva.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.HashMap;
-import java.util.Map;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.List;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
-    private EditText mEditTextName;
-    private EditText mEditTextEmail;
-    private EditText mEditTextPassword;
-    private Button mButtonRegister;
-
-    //VARIABLES DE LOS DATOS QUE VAMOS A REGISTRAR
-    private String name = "";
-    private String email = "";
-    private String password="";
-
-    private FirebaseAuth mAuth;
-    DatabaseReference mDatabase;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mAuth=FirebaseAuth.getInstance();
-        mDatabase=FirebaseDatabase.getInstance().getReference();
-
-        mEditTextName =(EditText) findViewById(R.id.editTextName);
-        mEditTextEmail=(EditText) findViewById(R.id.editTextEmail);
-        mEditTextPassword = (EditText) findViewById(R.id.editTextPassword);
-        mButtonRegister = (Button)findViewById(R.id.btnRegister);
-
-        mButtonRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                name=mEditTextName.getText().toString();
-                email=mEditTextEmail.getText().toString();
-                password=mEditTextPassword.getText().toString();
-
-                if(!name.isEmpty() && !email.isEmpty() && !password.isEmpty()){
-                    if(password.length() >= 6){
-                        registerUser();
-                        
-                    }else{
-                        Toast.makeText(MainActivity.this, "El password debe tener al menos 6 caracteres", Toast.LENGTH_SHORT).show();
-                    }
-
-                }
-                else{
-                    Toast.makeText(MainActivity.this, "Debe completar los campos", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
+        DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference();
 
 
+    }
+
+    public void iniciarSesion(View view){
+
+        List<AuthUI.IdpConfig> listaProveedores =
+                Arrays.asList(
+                        new AuthUI.IdpConfig.EmailBuilder().build(),
+                        new AuthUI.IdpConfig.GoogleBuilder().build()
+
+                );
+
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(listaProveedores)
+                        .build(),
+                1);
 
 
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
-        FirebaseUser currentUser = mAuth.getCurrentUser();
-        updateUI(currentUser);
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    }
+        if(requestCode == 1){
+            if(resultCode == RESULT_OK){
+                Log.d("infoApp","inicio de sesion exitoso");
+                Intent intent = new Intent(this, ProfileActivity.class);
+                startActivity(intent);
+            } else {
 
-    private void registerUser(){
-        mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Map<String,Object> map = new HashMap<>();
-                    map.put("name",name);
-                    map.put("email",email);
-                    map.put("password",password);
-
-                    String id=mAuth.getCurrentUser().getUid();
-
-                    mDatabase.child("Users").child(id).setValue(map).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task2) {
-                            if(task2.isSuccessful()){
-                                startActivity(new Intent(MainActivity.this, ProfileActivity.class ));
-                                finish();
-                            }else{
-                                Toast.makeText(MainActivity.this, "No se pudieron crear los datos correctamente", Toast.LENGTH_SHORT).show();
-                            }
-
-                        }
-                    });
-
-                }else{
-                    Toast.makeText(MainActivity.this, "No se pudo registrar este usuario", Toast.LENGTH_SHORT).show();
-                }
+                Log.d("infoApp","inicio erroneo");
             }
-        });
+
+        }
 
     }
+
+    public void abrirRegistrosActivity(View view){
+
+        Intent intent = new Intent(this, RegistroActivity.class);
+        startActivity(intent);
+
+
+    }
+
+
+
 }
