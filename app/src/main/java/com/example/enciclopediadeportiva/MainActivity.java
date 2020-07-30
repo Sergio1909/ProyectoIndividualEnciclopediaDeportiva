25812661042909d870e06155e364bdef326cb37d
Sergio Muro <sergio.muroc@pucp.pe>
2020-07-30 14:41:33 -0500

    arreglo recycler view

--- app/src/main/java/com/example/enciclopediadeportiva/MainActivity.java
+++ app/src/main/java/com/example/enciclopediadeportiva/MainActivity.java
@@ -26,9 +26,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
+
 
 import java.io.File;
 import java.io.FileInputStream;
